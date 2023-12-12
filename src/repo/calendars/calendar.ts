32ed7546ee06273efa1dbacbc60246dd18725df0
RyanCodesHard <ryan.taw96@gmail.com>
2023-12-13 01:42:20 +0800

    updated calendar api

--- src/repo/calendars/calendar.ts
+++ src/repo/calendars/calendar.ts
@@ -2,7 +2,7 @@ import { QueryResult } from 'pg'
 import handler from '../db_handler'
 
 const get_all_calendars = () : Promise<QueryResult<any>>=> {
-  return handler("SELECT * FROM calendars")
+  return handler("SELECT * FROM calendar")
 }
 
 export {get_all_calendars}
