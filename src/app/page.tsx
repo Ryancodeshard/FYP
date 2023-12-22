4fbec62bbdb1f1ae1967bf2bf383f080ee6d85c2
RyanCodesHard <ryan.taw96@gmail.com>
2023-12-22 23:54:38 +0800

    added ignore .next and .yarn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,44 @@
-'use client'
+"use client";
 
 import LoginBox from "@/_components/Login";
-import { useState } from "react";
+import { Box, Typography } from "@mui/material";
+import { Dispatch, SetStateAction, useState } from "react";
+
+async function FetchAllEvents(setEvent: Dispatch<SetStateAction<any>>) {
+  try {
+    const response = await fetch("/api/event");
+    const data = await response.json();
+    console.log("All events:", data);
+    setEvent(data[0].name);
+  } catch (error) {
+    console.error("Error fetching API:", error);
+  }
+}
+
+async function FetchInfo() {
+  try {
+    const response = await fetch("/api/db_info");
+    const data = await response.json();
+    console.log("GET request response:", data);
+  } catch (error) {
+    console.error("Error fetching API:", error);
+  }
+}
 
 export default function Home() {
-  async function FetchInfo() {
-    try {
-      const response = await fetch('/api/db_info');
-      const data = await response.json();
-      console.log('GET request response:', data);
-    } catch (error) {
-      console.error('Error fetching API:', error);
-    }
-  }
-  const [ event, setEvent] = useState([])
+  const [event, setEvent] = useState([]);
 
-  async function FetchAllEvents() {
-    try {
-      const response = await fetch('/api/event');
-      const data = await response.json();
-      console.log('All events:', data);
-      setEvent(data[0].name)
-    } catch (error) {
-      console.error('Error fetching API:', error);
-    }
-  }
   return (
-    <>
-      {/* Home
-      <button onClick={()=>FetchInfo()}>Click me</button>
-      <div>
-      <button onClick={()=>FetchAllEvents()}>Get all events</button>
-      {event}
-      </div> */}
-      <LoginBox/>
-    </>
-  )
+    <Box>
+      <Typography>Home</Typography>
+      <button onClick={() => FetchInfo()}>Click me</button>
+      <Box>
+        {/* <button onClick={() => FetchAllEvents((setEvent = setEvent))}>
+          Get all events
+        </button> */}
+        {event}
+      </Box>
+      {/* <LoginBox/> */}
+    </Box>
+  );
 }
